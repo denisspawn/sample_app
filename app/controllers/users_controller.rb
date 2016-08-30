21f399089ffdd35407f693c44feb5afe72390a74
spawn <denisgaiciuk@gmail.com>
2016-08-30 17:50:42 +0300

    Add form for adding users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,27 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # debugger # запускает консольный режим отладки. Приложение не запустится, пока не пройдет всю отладку, или сочетанием клавиш CTRL+D
+    # puts @user.attributes.to_yaml
   end
+
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end

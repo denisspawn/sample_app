9c85b37c03efa51e152f49bc357b7337d4b5d2f0
spawn <denisgaiciuk@gmail.com>
2016-09-08 17:26:33 +0300

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       # Handle a successful save.
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # This is because Rails automatically infers from redirect_to @user that we want to redirect to user_url(@user)
